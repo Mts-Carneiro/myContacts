0f22edec105fa1d7a02c3921bb280f82d84974fe
Mts-Carneiro <mtscrn21@gmail.comgit config --global user.email mtscrn21@gmail.com>
2023-03-22 20:34:19 -0300

    Add: cors

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,13 @@ import "reflect-metadata"
 import { handleErrors } from "./erros"
 import contactsRoutes from "./routes/contacts.routes"
 import userRoutes from "./routes/user.routes"
+import cors from 'cors'
 
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use('/user', userRoutes)
 app.use('/contact', contactsRoutes)
 
