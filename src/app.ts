963dd50ac2eeba0816bd4fabe79d97ac0dd27c93
Mts-Carneiro <mtscrn21@gmail.comgit config --global user.email mtscrn21@gmail.com>
2023-03-22 20:50:07 -0300

    fix: app functions

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,35 @@
-import express from "express"
-import "express-async-errors"
-import "reflect-metadata"
-import { handleErrors } from "./erros"
-import contactsRoutes from "./routes/contacts.routes"
-import userRoutes from "./routes/user.routes"
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
+import "express-async-errors";
+import "reflect-metadata";
+import { Response, Request, NextFunction } from "express";
 
+import contactsRoutes from "./routes/contacts.routes";
+import userRoutes from "./routes/user.routes";
+import { handleErrors } from "./erros";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-  
-app.use(cors())
-app.use('/user', userRoutes)
-app.use('/contact', contactsRoutes)
+app.use(express.json());
+app.use(cors());
+app.use(setupCORSHeaders);
+app.use("/user", userRoutes);
+app.use("/contact", contactsRoutes);
 
-app.use(handleErrors)
+app.use(handleErrors);
 
-export default app
+process.on("unhandledRejection", (err) => {
+  console.error("Unhandled rejection:", err);
+  process.exit(1);
+});
+
+function setupCORSHeaders(req: Request, res: Response, next:NextFunction) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+}
+
+export default app;
