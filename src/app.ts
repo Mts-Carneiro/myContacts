32cf6afa67205869b38080d1c3b18dcf40e98e92
Mts-Carneiro <mtscrn21@gmail.comgit config --global user.email mtscrn21@gmail.com>
2023-03-22 22:41:33 -0300

    fix: app.cors

--- src/app.ts
+++ src/app.ts
@@ -11,11 +11,18 @@ const app = express();
 
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Credentials', "true");
+    next();
+});
 
 app.use("/user", userRoutes);
 app.use("/contact", contactsRoutes);
