0f2daf939231d549ca11b31ed578c11c7ebe3aaa
Mts-Carneiro <mtscrn21@gmail.comgit config --global user.email mtscrn21@gmail.com>
2023-03-22 22:34:15 -0300

    fix: app.cors

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import "express-async-errors";
 import "reflect-metadata";
-import { Response, Request, NextFunction } from "express";
 
 import contactsRoutes from "./routes/contacts.routes";
 import userRoutes from "./routes/user.routes";
@@ -11,8 +10,13 @@ import { handleErrors } from "./erros";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
-app.use(setupCORSHeaders);
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
 app.use("/user", userRoutes);
 app.use("/contact", contactsRoutes);
 
@@ -22,14 +26,5 @@ process.on("unhandledRejection", (err) => {
   console.error("Unhandled rejection:", err);
   process.exit(1);
 });
-
-function setupCORSHeaders(req: Request, res: Response, next:NextFunction) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-}
 
 export default app;
