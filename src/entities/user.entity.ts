63fd0e176fe50292670cc4c9bf0a2f5224e48deb
Mts-Carneiro <mtscrn21@gmail.comgit config --global user.email mtscrn21@gmail.com>
2023-03-25 16:08:33 -0300

    fix:entities

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -19,7 +19,7 @@ class User {
     @CreateDateColumn()
     createdAt: Date
 
-    @OneToMany(() => Contact, (contact) => contact.user, {cascade:true})
+    @OneToMany(() => Contact, (contacts) => contacts.user, {cascade:true})
     contacts: Contact[]
 
 }
